01b5a6a815328f4a6bd462127b978a65d852d415
bhaz420 <127907510+bhaz420@users.noreply.github.com>
2023-04-12 12:23:59 -0600

    Add files via upload

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,5 @@
+alert("Hello, world!"); // Displays an alert dialog with the message "Hello, world!"
+
+const name = "John";
+const age = 25;
+alert(`My name is ${name} and I'm ${age} years old.`); // Displays an alert dialog with the message "My name is John and I'm 25 years old."
