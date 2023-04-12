5d71babbaf9e03fdeaaa81ec7aabc7aa84b62207
bhaz420 <127907510+bhaz420@users.noreply.github.com>
2023-04-12 14:08:10 -0600

    Update Basic_JavaScript_1.js

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,5 +1,4 @@
-alert("Hello, world!"); // Displays an alert dialog with the message "Hello, world!"
-
+<script src="basic_javascript_1. js"></script>
 const name = "John";
 const age = 25;
 alert(`My name is ${name} and I'm ${age} years old.`); // Displays an alert dialog with the message "My name is John and I'm 25 years old."
