21a2dcdc804325716e3b147a7837a5a0d536a3f1
bhaz420 <127907510+bhaz420@users.noreply.github.com>
2023-04-13 12:06:34 -0600

    Update Basic_JavaScript_1.js

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -2,3 +2,6 @@
 const name = "John";
 const age = 25;
 alert(`My name is ${name} and I'm ${age} years old.`); // Displays an alert dialog with the message "My name is John and I'm 25 years old."
+// Add a click event listener to the element
+heading.addEventListener("click", function() {
+  alert("You clicked on the heading!");
